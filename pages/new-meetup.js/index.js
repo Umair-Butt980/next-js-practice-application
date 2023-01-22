7bbeb86204607e0d580440118c53396fcf636616
UmairButt <umairamjadbutt1@gmail.com>
2023-01-22 18:38:40 +0500

    till new meetup working fine

--- pages/new-meetup.js/index.js
+++ pages/new-meetup.js/index.js
@@ -1,10 +1,16 @@
+import Layout from "@/components/layout/layout";
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  const addMeetupHandler = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
     console.log(enteredMeetupData);
   };
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Layout>
+      {" "}
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Layout>
+  );
 }
 
 export default NewMeetupPage;
