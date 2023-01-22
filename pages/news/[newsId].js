df55fdbb16bfa4963a3dfa575ac88cfb3956b3a0
UmairButt <umairamjadbutt1@gmail.com>
2023-01-22 15:55:27 +0500

    aded the dynamic routing

---
+++ pages/news/[newsId].js
@@ -0,0 +1,3 @@
+export default function DetailsPage() {
+  return <h1>THis is the Details page </h1>;
+}
--- pages/news/details.js
+++
@@ -1,5 +0,0 @@
-// ourDomain.com/news/details
-
-export default function DetailPage() {
-  <h1> These are the details of the news page </h1>;
-}
