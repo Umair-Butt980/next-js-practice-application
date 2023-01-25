a9e1091c97b02ad208a019dade74f78ea77fdf64
UmairButt <umairamjadbutt1@gmail.com>
2023-01-25 15:01:33 +0500

    added the get static props

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,20 @@ function meetUpDetails() {
   );
 }
 
+export async function getStaticProps(context){
+    let meetUpId = context.params.meetupId;
+    console.log(meetUpId,"<Chekcing>");
+    return {
+      props: {
+        meetupData: {
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/9/93/Lahore_collage.jpg",
+          title: "Heavy Place for rainy days",
+          address: "Checking street Opposite bypass",
+          description: "This si the place to be",
+        },
+      },
+    };
+}
+
 export default meetUpDetails;
